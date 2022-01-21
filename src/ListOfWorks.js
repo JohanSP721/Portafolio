925e0e4bff15813ba28200a7548b78b093028a4b
Johan Sarmiento Peña <johansarmiento_21@live.com>
2022-01-21 18:04:09 -0500

    list of works exportado

--- src/ListOfWorks.js
+++ src/ListOfWorks.js
@@ -1,4 +1,4 @@
-const ListOfWorks = [
+export const ListOfWorks = [
 	{id:1, video: "../assets/static/RickAndMortyAngular.mp4", name: "RickAndMorty(Angular)", description: "un pequeño proyecto con funcionalidades de búsqueda, favoritos, notificaciones y lazy loading usando Angular, GraphQL y la API de rick y morty(rickandmortyapi.com)", code: ""},
 	{id:2, video: "", name: "", description: ""},
 	{id:3, video: "", name: "", description: ""},
@@ -7,4 +7,4 @@ const ListOfWorks = [
 	{id:6, video: "", name: "", description: ""},
 	{id:7, video: "", name: "", description: ""},
 	{id:8, video: "", name: "", description: ""}
-]
+];
